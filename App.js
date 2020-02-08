4ac8f8802cc4978ceea594e229d4e11945d0d279
Alan Krajina <alankrajina88@gmail.com>
2020-02-08 18:34:42 +0000

    delete item comment

--- App.js
+++ App.js
@@ -12,19 +12,19 @@ export default function App() {
   }
 
 // create state to store enteredTextGoal from TextInput:
-  const [goals, setGoals] = useState([])    // goals new STATE - empty array
+  const [goals, setGoals] = useState([])    // goals new STATE - empty ARRAY
                                             // setGoals function to set state
 
-  const addGoalHandler = () => {  // onPress updates GOALS STATE with new objects
+  const addGoalHandler = () => {  // onPress updates GOALS STATE array with new objects
     setGoals(currentGoals =>      // -> current - previous GOALS STATE
-      [...currentGoals, {id: Math.random().toString(), value: enteredTextGoal}]) // combines previous and newly created object
-    }                                                                                 // from enteredTextGoal STATE (input string)
+      [...currentGoals, {id: Math.random().toString(), value: enteredTextGoal}]) // combines previous and newly created object into GOALS ARRAY
+    }                                                                      // newly created -> from enteredTextGoal STATE (input string)
 
   const deleteItem = itemID => {  // itemID is the props.id we send to <GoalItem/> (every newly created row has its own id upon creation)
     setGoals(currentGoals => {      // -> current - previous GOALS STATE
-      return currentGoals.filter(item => item.id !== itemID)  // checks current GOALS and returns all 
+      return currentGoals.filter(item => item.id !== itemID)  // checks current GOALS and returns NEW ARRAY (GOALS)
     })                                       // items in goals that dont match to the clicked one
-  }                                   //  (clicked has specific itemID & ALL ids in GOALS ARRAY)
+  }                                //  (clicked has specific itemID from props & ALL ids in GOALS ARRAY)
 
 
   return (
